6c15a96cac1f612b7347dd1f3cdf81c66c9dcd05
jinook929 <jinook929@gmail.com>
2021-04-14 20:12:21 -0400

    commit tree 7fa4f36e970dd248ac08be8bfc92b7ef792a62e0

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 11
+const num4 = 7
+
+let multiply = num1 * num2
+let random = Math.round(Math.random() * 10)
+let mod = num3 % num4
+let max = Math.max(16,17,18,19,20)
